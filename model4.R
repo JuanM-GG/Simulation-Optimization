67b5b0c6074919be1e22146f82f7f421b642e536
JuanM <58197473+JuanM-GG@users.noreply.github.com>
2020-06-21 17:58:40 -0500

    load model4

---
+++ model4.R
@@ -0,0 +1,52 @@
+# Growth without inhinition and with cell death
+# Juan Manuel
+# 21/06/20
+# Note: This model donesn't consider any type of inhibition and also 
+# it considers cell death. Because some processes can danger the cell, e.g.,
+# the agitation and temperature changes inside the bioreactor, for these 
+# reasons we can consider that it is important consider the rate at which 
+# the cells death
+######################################################################
+
+# Load libraries
+library(deSolve)
+library(GA)
+library(tidyverse)
+
+#Set the model
+model <-function(time, parms, state) {
+        with(as.list(c(parms,state)),{
+                
+                #  reaction kinetic rates
+                v <- vmax*s/(ks+s)
+                
+                # differential equations
+                dx <- v*x - kd*x
+                ds <- -(1/yxs)*v*x
+                dp <- ypx*v*x
+                return(list(c(dx,ds,dp)))
+        })
+}
+
+# Set parameters
+p <- c(vmax = 0.5, ks = 80, yxs = 0.8, ypx = 12, kd = 0.1)
+
+# Set initial conditions
+s <- c(x = 0.2, s = 40, p = 0)
+
+# Run the model
+out <- ode(y = s,
+           times = seq(0,60),
+           func = model,
+           parms = p,
+           method = "rk4")
+out <- as.data.frame(out)
+
+# Show output
+ggplot(data = out) + 
+        geom_point(mapping = aes(x = time, y = s), color = "red3") + 
+        geom_point(mapping = aes(x = time, y = x), color = "midnightblue") +
+        geom_point(mapping = aes(x = time, y = p),color = "yellowgreen") + 
+        labs(title = "simulation", x = "time (h)", y = "Density (g/L)")+
+        theme_bw()
+
